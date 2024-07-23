826501881b5d329b12748a18c2c1a102d92105ae
Half-Engineer <nisarsafi2001@gmail.com>
2024-07-23 13:52:54 +0500

    testing for deploying

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<!-- src/routes/+layout.svelte -->
+<script>
+    import 'bulma/css/bulma.min.css';
+    import '@fortawesome/fontawesome-free/css/all.min.css';
+  </script>
+  
+  <slot />
+  
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from "@sveltejs/adapter-static";
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
   kit: {
     adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      precompress: false,
-      strict: true,
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false
     }),
+    paths: {
+      base: dev ? '' : '/dpractice'
+    }
   },
+  preprocess: sveltePreprocess()
 };
